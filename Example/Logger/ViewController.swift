c41e68e01a4443a8d7d380508ee35b15f43e4368
Avinash <avinash@cssoftsolutions.com>
2025-04-17 12:29:12 +0530

    Initial commit

---
+++ Example/Logger/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Logger
+//
+//  Created by avinash@cssoftsolutions.com on 04/17/2025.
+//  Copyright (c) 2025 avinash@cssoftsolutions.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
